717e9ceed819cd40310edec38d0aa3d418897923
Artem14072003 <artem_krivenko_2019@mail.ru>
2026-08-24 18:29:46 +0300

    The skill section is completed

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -19,11 +19,38 @@ const SkillSection = () => {
                     </p>
                 </div>
                 <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8"}>
-                    {TECHNICAL_SKILLS.map(() =>
+                    {TECHNICAL_SKILLS.map(({id, label, techStack, text, icon: Icon}) =>
                         <div
-                            className={'$'}
+                            key={id}
+                            className={'skillGlassPanel p-10 rounded-xl relative overflow-hidden group'}
                         >
-
+                            <div
+                                className={'absolute inset-0 bg-linear-to-br from-oyster-bay/5 to-transparent ' +
+                                    'opacity-0 group-hover:opacity-100 transition-opacity duration-500'}
+                            />
+                            <div
+                                className={'flex items-center gap-4 mb-8'}
+                            >
+                                <Icon className={`${text} text-3xl shrink-0`}/>
+                                <h3 className={'text-[32px] leading-[1.3] tracking-[-0.01em] font-semibold text-mercury'}>{label}</h3>
+                            </div>
+                            <div className={'space-y-6'}>
+                                {techStack.map((skill) => (
+                                    <div key={skill.label}>
+                                        <div
+                                            className={'flex justify-between mb-2 text-xs leading-[1.4] ' +
+                                                'tracking-widest font-medium'}
+                                        >
+                                            <span className={'text-mercury'}>{skill.label}</span>
+                                            <span className={`${text} font-bold`}>{skill.percentage}</span>
+                                        </div>
+                                        <div className={'h-1 bg-cape-cod rounded-full overflow-hidden'}>
+                                            <div className={`h-full bg-linear-to-r from-mauve to-cyan 
+                                                ${skill.width} progressBarFill`}/>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     )}
                 </div>
